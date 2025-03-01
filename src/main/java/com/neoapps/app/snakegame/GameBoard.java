a23e7cb277392029fb49809a13f86a788ead22b5
Neoly Moreno <neolyamr@gmail.com>
2025-02-28 21:48:14 -0500

    - Move images package - Add topBar and refactor - Add styles.css

--- src/main/java/com/neoapps/app/snakegame/GameBoard.java
+++ src/main/java/com/neoapps/app/snakegame/GameBoard.java
@@ -1,62 +1,130 @@
 package com.neoapps.app.snakegame;
 
 import javafx.animation.AnimationTimer;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 
 public class GameBoard extends Stage {
 
     private final int WIDTH = 600;
-    private final int HEIGHT = 600;
+    private final int HEIGHT = 640;
+
+    private final int CANVAS_WIDTH = 600;
+    private final int CANVAS_HEIGHT = 600;
 
     private Snake snake;
 
     private Apple apple;
 
+    private int score = 0;
+
+    private boolean isStopped = false;
+
+    /**
+     * Confirms if the last keyboard event (direction) has been updated on screen
+     */
+    private boolean isFrameUpdated = false;
+
     private GraphicsContext graphicsContext;
 
-    AnimationTimer animationTimer;
+    private AnimationTimer animationTimer;
+
+    private Label lblCurrentScore;
 
     public GameBoard() {
         setComponents();
         setTitle("Sneak Game");
+        setResizable(false);
         startAnimation();
     }
 
     public void setComponents() {
 
         snake = new Snake();
-        snake.increaseSize(WIDTH / 2, HEIGHT / 2);
-        snake.increaseSize((WIDTH / 2) + Snake.getTileSize(), (HEIGHT / 2) + Snake.getTileSize());
+        snake.increaseSize(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+        snake.increaseSize((CANVAS_WIDTH / 2) + Snake.getTileSize(), (CANVAS_HEIGHT / 2) + Snake.getTileSize());
 
-        apple = new Apple(WIDTH, HEIGHT, Snake.getTileSize());
+        apple = new Apple(CANVAS_WIDTH, CANVAS_HEIGHT, Snake.getTileSize());
 
-        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        Canvas canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
         graphicsContext = canvas.getGraphicsContext2D();
 
-        VBox vBox = new VBox(canvas);
-
-        Scene scene = new Scene(vBox, WIDTH, HEIGHT);
-        scene.setOnKeyPressed(event -> {
-            KeyCode key = event.getCode();
-            if (key == KeyCode.UP && !snake.getDirection().equals("DOWN")) snake.setDirection("UP");
-            if (key == KeyCode.DOWN && !snake.getDirection().equals("UP")) snake.setDirection("DOWN");
-            if (key == KeyCode.LEFT && !snake.getDirection().equals("RIGHT")) snake.setDirection("LEFT");
-            if (key == KeyCode.RIGHT && !snake.getDirection().equals("LEFT")) snake.setDirection("RIGHT");
-        });
+        Scene scene = getScene(canvas);
         setScene(scene);
         scene.getRoot().requestFocus();
+    }
+
+    private Scene getScene(Canvas canvas) {
+
+        Label lblScore = new Label("Score ");
+        lblScore.getStyleClass().add("labelScore");
+
+        lblCurrentScore = new Label("0");
+        lblCurrentScore.setFont(new Font(15));
+        lblCurrentScore.setStyle("-fx-text-fill:green;");
+
+        HBox topBar = new HBox(lblScore, lblCurrentScore);
+        topBar.setAlignment(Pos.CENTER_RIGHT);
+        topBar.setPadding(new Insets(10));
+        topBar.setSpacing(20);
+
+        VBox vBox = new VBox(topBar, canvas);
+        vBox.setAlignment(Pos.BOTTOM_CENTER);
+        VBox.setVgrow(canvas, Priority.ALWAYS);
+
+        Scene scene = new Scene(vBox, WIDTH, HEIGHT);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/styles.css")).toExternalForm());
+        scene.setOnKeyPressed(event -> {
+
+            KeyCode key = event.getCode();
+
+            if (key == KeyCode.P) {
+                toggleAnimation();
+            } else if (isFrameUpdated) {
+                changeSnakeDirection(event.getCode());
+                isFrameUpdated = false;
+            }
+        });
+        return scene;
+    }
+
+    public void toggleAnimation() {
+        if (isStopped) {
+            animationTimer.start();
+        } else {
+            animationTimer.stop();
+        }
+        isStopped = !isStopped;
+    }
+
+    public void changeSnakeDirection(KeyCode key) {
+        switch (key) {
+            case KeyCode.UP -> {
+                if (!snake.getDirection().equals("DOWN")) snake.setDirection("UP");
+            }
+            case KeyCode.DOWN -> {
+                if (!snake.getDirection().equals("UP")) snake.setDirection("DOWN");
+            }
+            case KeyCode.RIGHT -> {
+                if (!snake.getDirection().equals("LEFT")) snake.setDirection("RIGHT");
+            }
+            case KeyCode.LEFT -> {
+                if (!snake.getDirection().equals("RIGHT")) snake.setDirection("LEFT");
+            }
+        }
     }
 
     public void startAnimation() {
@@ -67,13 +135,12 @@ public class GameBoard extends Stage {
             @Override
             public void handle(long now) {
 
-                if (now - lastUpdate >= 100_000_000) { // Cada 100ms
-
+                if (now - lastUpdate >= 100_000_000) { // Every 100ms
                     update();
                     draw();
                     lastUpdate = now;
+                    isFrameUpdated = true;
                 }
-
             }
         };
 
@@ -81,7 +148,10 @@ public class GameBoard extends Stage {
     }
 
     public void draw() {
-        graphicsContext.clearRect(0, 0, WIDTH, HEIGHT);
+        graphicsContext.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        graphicsContext.setFill(Color.BLACK);
+        graphicsContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
         graphicsContext.setFill(Color.GREEN);
         for (int[] location : snake.getBody()) {
@@ -104,16 +174,18 @@ public class GameBoard extends Stage {
             case "LEFT" -> newX -= Snake.getTileSize();
             case "RIGHT" -> newX += Snake.getTileSize();
         }
-
-        if (newX < 0 || newX >= WIDTH || newY < 0 || newY >= HEIGHT || hasCollided(newX, newY)) {
+        if (newX < 0 || newX >= CANVAS_WIDTH || newY < 0 || newY >= CANVAS_HEIGHT || hasCollided(newX, newY)) {
             animationTimer.stop();
             showGameOverMessage();
         }
+
 
         snake.increaseSize(newX, newY);
 
         if (newX == apple.getX() && newY == apple.getY()) {
             spawnApple();
+            score++;
+            lblCurrentScore.setText(String.valueOf(score));
         } else {
             snake.decreaseSize();
         }
@@ -121,6 +193,7 @@ public class GameBoard extends Stage {
 
     public void restartGame() {
         snake.setDirection("RIGHT");
+        score = 0;
         setComponents();
         startAnimation();
     }
@@ -136,22 +209,30 @@ public class GameBoard extends Stage {
     }
 
     public void spawnApple() {
-        apple = new Apple(WIDTH, HEIGHT, Snake.getTileSize());
+        apple = new Apple(CANVAS_WIDTH, CANVAS_HEIGHT, Snake.getTileSize());
     }
 
     public void showGameOverMessage() {
 
         Label lblGameOver = new Label("Game Over");
         lblGameOver.setFont(new Font(30));
+        lblGameOver.setStyle("-fx-text-fill: White;");
+
+        Label lblFinalScore = new Label(String.valueOf(score));
+        lblFinalScore.setFont(new Font(20));
+        lblFinalScore.setStyle("-fx-text-fill: White;");
 
         Button btnTryAgain = new Button("Try Again");
-        btnTryAgain.setCursor(Cursor.HAND);
+        btnTryAgain.getStyleClass().add("button-black-green");
         btnTryAgain.setOnAction(event -> restartGame());
 
-        VBox vBox = new VBox(lblGameOver, btnTryAgain);
+        VBox vBox = new VBox(lblGameOver, lblFinalScore, btnTryAgain);
+        vBox.setStyle("-fx-background-color: black;");
+        vBox.setSpacing(20);
         vBox.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(vBox, WIDTH, HEIGHT);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/styles.css")).toExternalForm());
         setScene(scene);
 
     }
--- src/main/java/com/neoapps/app/snakegame/Main.java
+++ src/main/java/com/neoapps/app/snakegame/Main.java
@@ -2,26 +2,23 @@ package com.neoapps.app.snakegame;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
 public class Main extends Application {
 
-
     @Override
     public void start(Stage stage) {
 
         Button button = new Button("Let's go");
-        button.setFont(new Font(20));
-        button.setCursor(Cursor.HAND);
+        button.getStyleClass().add("button-black-green");
+
         button.setOnAction(event -> {
             GameBoard gameBoard = new GameBoard();
             stage.close();
@@ -29,20 +26,22 @@ public class Main extends Application {
 
         });
 
-        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/neoapps/app/snakegame/images/SnakeGameLogo.jpg")));
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/SnakeGameLogo.jpg")));
         ImageView imageView = new ImageView(image);
 
         VBox vBox = new VBox(imageView, button);
+        vBox.setStyle("-fx-background-color: black;");
         vBox.setAlignment(Pos.CENTER);
         vBox.setSpacing(10);
 
         Scene scene = new Scene(vBox, 600, 600);
+
         stage.setTitle("SnakeGame");
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/styles.css")).toExternalForm());
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
     }
-
 
     public static void main(String[] args) {
         launch();
